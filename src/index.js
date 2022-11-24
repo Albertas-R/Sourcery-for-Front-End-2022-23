95d60f638216ae2a67f43feb2fb5e13f6a83577f
violent-code <violent-intent@protonmail.com>
2022-11-24 03:48:55 +0200

    SR22S-4: restructure root files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { RouteManager } from './routes/routing';
+import './sass/global.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <RouteManager />
     </Router>
   </React.StrictMode>
 );
